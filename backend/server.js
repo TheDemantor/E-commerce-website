1147de8efa909c0d1ee2848b49460e7fa465193b
Vaibhav <emailtovaibhavgupta@gmail.com>
2023-08-07 19:19:44 +0530

    bb

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import path from 'path'
 import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config();
+// import dotenv from 'dotenv'
+// dotenv.config();
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
